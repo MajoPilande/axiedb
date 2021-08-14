e524621a1a89719981c748d1bad9c335cfe4c7af
MajoPilande <mjpilande@gmail.com>
2021-08-14 18:07:45 +0800

    test axie db

---
+++ src/pages/Customers.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+import Table from '../components/table/Table'
+
+import customerList from '../assets/JsonData/customers-list.json'
+
+const customerTableHead = [
+    '',
+    'name',
+    'Ronin',
+    'Manager Share',
+    'Isko Share',
+    'Total Slp',
+    'Average'
+]
+
+const renderHead = (item, index) => <th key={index}>{item}</th>
+
+const renderBody = (item, index) => (
+    <tr key={index}>
+        <td>{item.id}</td>
+        <td>{item.name}</td>
+        <td>{item.eth}</td>
+        <td>{item.managerShare}</td>
+        <td>{item.iskoShare}</td>
+        <td>{item.totalSlp}</td>
+        <td>{item.average}</td>
+    </tr>
+)
+
+const Customers = () => {
+    return (
+        <div>
+            <h2 className="page-header">
+                customers
+            </h2>
+            <div className="row">
+                <div className="col-12">
+                    <div className="card">
+                        <div className="card__body">
+                            <Table
+                                limit='10'
+                                headData={customerTableHead}
+                                renderHead={(item, index) => renderHead(item, index)}
+                                bodyData={customerList}
+                                renderBody={(item, index) => renderBody(item, index)}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Customers
